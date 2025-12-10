5ba9a9b575c6364b551ca52d4dbde27a84cb49ed
Dhruv13Korat <dkp86750@gmail.com>
2025-12-10 14:15:55 +1100

    configured sequeilize and create 3 models order,product and user

---
+++ Back-end/models/product.js
@@ -0,0 +1,25 @@
+'use strict';
+import Model from 'sequelize';
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    price: DataTypes.DECIMAL,
+    stock: DataTypes.INTEGER,
+    imageUrl: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
